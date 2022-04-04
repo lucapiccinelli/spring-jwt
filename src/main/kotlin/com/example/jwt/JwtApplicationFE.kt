d18e15c89d8405d3a704dd3b385eebd7c3d3d4e5
lucapiccinelli <luca.picci@gmail.com>
2022-04-04 10:59:14 +0200

    request for a token

--- src/main/kotlin/com/example/jwt/JwtApplicationFE.kt
+++ src/main/kotlin/com/example/jwt/JwtApplicationFE.kt
@@ -7,7 +7,12 @@ import com.nimbusds.jose.jwk.source.JWKSource
 import com.nimbusds.jose.proc.SecurityContext
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.support.beans
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -16,12 +21,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
+import org.springframework.security.oauth2.core.oidc.OidcScopes
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.config.ClientSettings
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.exchange
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.router
 import java.time.Duration
@@ -35,9 +45,72 @@ fun main(args: Array<String>) {
     val hostname = "127.0.0.1"
     val port = "8080"
     val rootUri = "http://$hostname:$port"
+    val tokenRoutes = "/bla"
+    val redirectUri = "$tokenRoutes/tokenCallback"
+    val tokenUri = "$tokenRoutes/token"
 
     runApplication<JwtApplicationFE>(*args){
         addInitializers(beans {
+            bean {
+                val restTemplateBuilder: RestTemplateBuilder = ref()
+                val rest = restTemplateBuilder.rootUri(rootUri).build()
+
+                router {
+                    POST(tokenUri){
+                        val authorizeUrl = "/oauth2/authorize?response_type=code&client_id=messaging-client&scope=openid&redirect_uri=$rootUri$redirectUri&state=1234"
+
+                        val headers = HttpHeaders().apply {
+                            contentType = MediaType.APPLICATION_FORM_URLENCODED
+                            setBasicAuth("user1", "password")
+                        }
+                        val request = RequestEntity
+                            .get(authorizeUrl)
+                            .headers(headers)
+                            .build()
+
+                        try {
+                            rest.exchange<String>(request)
+                        } catch (ex: HttpClientErrorException.NotFound){
+                            val strings = ex.responseHeaders!!["Set-Cookie"]
+                            headers.add("Cookie", strings!![0])
+                            rest.exchange(RequestEntity
+                                .get(authorizeUrl)
+                                .headers(headers)
+                                .build())
+                        }.let { responseEntity ->
+                            ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(responseEntity.body!!)
+                        }
+                    }
+
+                    GET(redirectUri){
+                        val code: String? = it.param("code").orElse(null)
+                        val state: String? = it.param("state").orElse(null)
+
+                        val headers = HttpHeaders().apply {
+                            contentType = MediaType.APPLICATION_FORM_URLENCODED
+                            setBasicAuth("messaging-client", "secret")
+                        }
+                        val obj: MultiValueMap<String, String> = LinkedMultiValueMap<String, String>().apply {
+                            add("grant_type", "authorization_code")
+                            add("code", code)
+                            add("redirect_uri", "$rootUri$redirectUri")
+                        }
+                        val request = RequestEntity
+                            .post("/oauth2/token")
+                            .headers(headers)
+                            .body(obj)
+
+                        val responseEntity: ResponseEntity<String> = rest.exchange(request)
+
+                        ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(responseEntity.body!!)
+                    }
+                }
+            }
+
             bean {
                 router {
                     GET("/secured/test") {
@@ -65,6 +138,7 @@ fun main(args: Array<String>) {
                     .authorizeRequests {
                         it.anyRequest().authenticated()
                     }
+                    .csrf().ignoringAntMatchers(tokenUri).and()
                     .httpBasic().and()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                     .build()
@@ -86,8 +160,10 @@ fun main(args: Array<String>) {
                     .clientId("messaging-client")
                     .clientSecret(encoder.encode("secret"))
                     .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                    .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                    .redirectUri("$rootUri/api/xxx")
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                    .redirectUri("$rootUri$redirectUri")
+                    .scope(OidcScopes.OPENID)
                     .scope("efc")
                     .clientSettings(
                         ClientSettings.builder()
